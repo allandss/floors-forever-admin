673587221c60a5f6c682b7b9bf999b6edf9e5556
Allan Santos <allandssilva@gmail.com>
2020-04-22 00:49:22 -0300

    up

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Nav from './nav';
 import { Header } from '../styles';
-import Logo from '../assets/images/logo.jpeg';
+import Logo from '../assets/images/logo-clean.png';
 
 export default function header() {
   return (
     <Header>
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <img src={Logo} alt="Logo" className="logo" />          
-            <div className="navbar-collapse">
-              <Nav />
-            </div>
+          <a href="/">
+            <img src={Logo} alt="Logo" className="logo" />
+          </a>          
+          <div className="navbar-collapse">
+            <Nav />
+          </div>
         </nav>
       </div>
     </Header>
