8e4ba204004b688c9a89821faa74b5c6956e3be5
Allan Santos <allandssilva@gmail.com>
2020-05-03 12:59:38 -0300

    up

--- src/services/serverFile.js
+++ src/services/serverFile.js
@@ -1,4 +1,4 @@
 //const serverFile = 'http://localhost:3333/files/';
-const serverFile = 'https://floors-forever-api.herokuapp.com/';
+const serverFile = 'https://floors-forever-api.herokuapp.com/files/';
 
 export default serverFile;
